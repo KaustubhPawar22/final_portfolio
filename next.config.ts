2778cb0ed2e470df1ea721acd485dfb6e3b98d20
Kaustubh Pawar <kaustubhpawar500@gmail.com>
2025-08-20 18:22:41 +0530

    fixes and optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com"], // allow images from Unsplash
